ee876195fc368eb139f5fc71dfd87f7a6ea15a42
=Memo <=chepkemoicynthia4@gmail.com>
2024-04-07 15:23:21 +0300

    Completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content  # Use content instead of text to get bytes
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            response_text = response_body.decode('utf-8')  # Decode bytes to string
+            return json.loads(response_text)  # Parse string as JSON
+        else:
+            return None
